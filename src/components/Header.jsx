0dfb6b762b66f19a84e3e7948c2dc474e6d231d0
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-12 02:58:21 +0400

    header component create

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () =>{
+   return (
+    <>
+    <div className="bg-[url(../public/Background.png)] bg-cover  bg-no-repeat bg-center">
+          <h1 className="h-[700px]">salam</h1>
+    </div>
+    </>
+   )
+}
+export default Header

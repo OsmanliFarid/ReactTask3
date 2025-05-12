a90c83895317d6336494e661ba78ae696cfea980
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-12 16:56:02 +0400

    index css tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Fresh from './components/Fresh'
+import Question from './components/question'
+import BoxItems from './components/BoxItems'
 
 
 function App() {
   return (
-    <>
+    <div className='overflow-x-hidden'>
       <Navbar/>
       <Header />
-    </>
+      <Fresh/>
+      <BoxItems/>
+      <Question/>
+      
+    </div>
   )
 }
 

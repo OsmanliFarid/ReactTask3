69b3d0b5f6a0a24b00f8a37639acb704ec40ac9a
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-12 16:56:34 +0400

    Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,16 +9,16 @@ const Navbar = () =>{
     ]
  return(
     <>
-    <div className="grid grid-cols-[120px_1260px_130px] items-center h-[80px] m-[0px_20px]">
+    <div className="grid grid-cols-[80px_1280px_118px] items-center m-[0px_20px]  h-[80px] ">
         <div className="Logo">
             <img src="./public/logo.png" alt="" />
         </div>
-        <div className="font-[Sans]">
+        <div className="font-[Sans] hidden sm:block">
             {Links.map(({id,title,href},index) =>{
                 return <a className="ml-3.5 text-[#242424] text-[16px] leading-[24px] font-[600]"key={index} href={href}>{title}</a>
             })}
         </div>
-        <div className="LogBtn">
+        <div className="LogBtn hidden sm:block">
             <a href="#" className="inline-block border-2 border-[#067A46] p-[6px_35px] text-[#067A46]">Log in</a>
         </div>
     </div>
